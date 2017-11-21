baf67f8a585f89568a1e6a311f191aa8f9b513b4
Luis Herranz <luisherranz@gmail.com>
2017-11-21 18:11:56 +0100

    Refactor variables

--- injector/wp-pwa-injector.php
+++ injector/wp-pwa-injector.php
@@ -9,11 +9,14 @@ $listId = null;
 $page = null;
 $singleType = null;
 $singleId = null;
-$env = 'pre';
+$env = 'prod';
 $perPage = get_option('posts_per_page');
+$ssr = 'https://ssr.wp-pwa.com';
+$static = 'https://static.wp-pwa.com';
 
 if (is_home()) {
   $listType = 'latest';
+  $listId = 'post';
 } elseif (is_page() || is_single()) {
   $singleType = get_queried_object()->post_type;
   $singleId = get_queried_object()->ID;
@@ -22,11 +25,11 @@ if (is_home()) {
   if ((isset($queriedObject->show_in_rest)) && (isset($queriedObject->rest_base)) &&
   ($queriedObject->show_in_rest === true)) {
     $listType = 'latest';
-    $singleType = $queriedObject->rest_base;
+    $listId = $queriedObject->rest_base;
   }
 } elseif (is_tax()) {
-  $listType = 'taxonomy';
-  // $listId = get_queried_object()->term_id;
+  $listType = get_queried_object()->taxonomy;
+  $listId = get_queried_object()->term_id;
 } elseif (is_category()) {
   $listType = 'category';
   $listId = get_queried_object()->term_id;
@@ -51,7 +54,8 @@ if (is_home()) {
 
 if (is_paged()) {
   $page = get_query_var('paged');
-} elseif (is_home() || is_category() || is_tag() || is_author() || is_search() || is_date()) {
+} elseif (is_home() || is_category() || is_tag() || is_author() || is_search() || is_date() ||
+  is_tax() || is_post_type_archive()) {
   $page = 1;
 }
 
@@ -80,8 +84,6 @@ if (isset($_GET['ssr'])) {
   $ssr = $_GET['server'];
 } elseif (isset($settings['wp_pwa_ssr'])) {
   $ssr = $settings["wp_pwa_ssr"];
-} else {
-  $ssr = 'https://ssr.wp-pwa.com';
 }
 if (isset($_GET['static'])) {
   $static = $_GET['static'];
@@ -89,25 +91,16 @@ if (isset($_GET['static'])) {
   $static = $_GET['server'];
 } elseif (isset($settings['wp_pwa_static'])) {
   $static = $settings["wp_pwa_static"];
-} else {
-  $static = 'https://static.wp-pwa.com';
 }
 
-if ((isset($_GET['env']) && ($_GET['env'] === 'prod'))) {
-  $env = 'prod';
+if ((isset($_GET['env']) && ($_GET['env'] === 'pre'))) {
+  $env = 'pre';
 }
-
 ?>
 
 <?php if ($siteId && ($listType || $singleType)) { ?>
   <script type='text/javascript'>
-  window.['wp-pwa'] = {
-    siteId: '<?php echo $siteId; ?>',
-    <?php if ($listType) echo 'listType: \'' . $listType . '\',' ?>
-    <?php if ($listId) echo 'listId: \'' . $listId . '\',' ?>
-    <?php if ($singleType) echo 'singleType: \'' . $singleType . '\',' ?>
-    <?php if ($singleId) echo 'singleId: \'' . $singleId . '\',' ?>
-  }
+  window['wp-pwa'] = { siteId: '<?php echo $siteId; ?>',<?php if ($listType) echo ' listType: \'' . $listType . '\',' ?><?php if ($listId) echo ' listId: \'' . $listId . '\',' ?><?php if ($singleType) echo ' singleType: \'' . $singleType . '\',' ?><?php if ($singleId) echo ' singleId: \'' . $singleId . '\',' ?><?php if ($page) echo ' page: \'' . $page . '\',' ?> env: '<?php echo $env; ?>', perPage: '<?php echo $perPage; ?>', ssr: '<?php echo $ssr; ?>', static: '<?php echo $static; ?>', home: { title: '<?php echo $homeTitle; ?>', description: '<?php echo $homeDescription; ?>' } };
   <?php require(WP_PLUGIN_DIR . $GLOBALS['wp_pwa_path'] . '/injector/injector.js'); ?>
   </script>
 <?php } ?>
