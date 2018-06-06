941b63b4274d34bd6b69252dbd6d59b1ef507791
Luis Herranz <luisherranz@gmail.com>
2018-06-06 17:18:46 +0200

    Use ngrok for development

--- service-workers/OneSignalSDKWorker.js.php
+++ service-workers/OneSignalSDKWorker.js.php
@@ -8,10 +8,13 @@ require_once dirname(__FILE__) . '/../../../../wp-load.php';
 
 header('Service-Worker-Allowed: /');
 header('Content-Type: application/javascript');
+header('Cache-Control: no-cache');
 header('X-Robots-Tag: none');
 
 $settings = get_option('wp_pwa_settings');
-$siteId = $settings['wp_pwa_siteid'];
 ?>
-const siteId = "<?php echo $siteId; ?>";
+var siteId = "<?php echo$settings['wp_pwa_siteid']; ?>";
+var dynamicUrl = "https://007764b3.ngrok.io";
+var staticUrl = "https://007764b3.ngrok.io";
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
+importScripts('https://007764b3.ngrok.io/static/sw.js');
