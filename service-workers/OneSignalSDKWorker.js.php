f95405e39e9346b054a382d43a5acf7f19ee5894
Luis Herranz <luisherranz@gmail.com>
2018-06-08 16:58:39 +0200

    Update SW

--- service-workers/OneSignalSDKWorker.js.php
+++ service-workers/OneSignalSDKWorker.js.php
@@ -12,9 +12,25 @@ header('Cache-Control: no-cache');
 header('X-Robots-Tag: none');
 
 $settings = get_option('wp_pwa_settings');
+$isPageOnFront = get_option('page_on_front') !== '0';
+$type = $isPageOnFront ? 'page' : 'latest';
+$id = $isPageOnFront ? get_option('page_on_front') : 'post';
+if (!$isPageOnFront) $page = 1;
+$dynamicUrl = substr($settings['wp_pwa_ssr'], -1) === '/' ? $settings['wp_pwa_ssr'] : $settings['wp_pwa_ssr'] . '/';
+$staticUrl = substr($settings['wp_pwa_static'], -1) === '/' ? $settings['wp_pwa_static'] : $settings['wp_pwa_static'] . '/';
+$initialUrl = substr(site_url(), -1) === '/' ? site_url() : site_url() . '/';
 ?>
-var siteId = "<?php echo$settings['wp_pwa_siteid']; ?>";
-var dynamicUrl = "https://007764b3.ngrok.io";
-var staticUrl = "https://007764b3.ngrok.io";
+
+var preCacheFiles = [];
+
+var siteId = "<?php echo $settings['wp_pwa_siteid']; ?>";
+var dynamicUrl = "<?php echo $dynamicUrl; ?>";
+var staticUrl = "<?php echo $staticUrl; ?>";
+var perPage = <?php echo get_option('posts_per_page'); ?>;
+var type = "<?php echo $type; ?>";
+var id = "<?php echo $id; ?>";
+var initialUrl = "<?php echo $initialUrl; ?>";
+<?php if (isset($page)) echo 'var page = ' . $page . ';'; ?>
+
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
-importScripts('https://007764b3.ngrok.io/static/sw.js');
+importScripts('<?php echo $dynamicUrl; ?>static/sw.js');
