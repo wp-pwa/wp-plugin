bd7013f4d3d4b52cdf7a363f4342cf95eea29953
Eduardo Campaña <asrielo@gmail.com>
2018-08-10 11:43:34 +0200

    fix(html-purifier): install htmlpurifier-html5 library for html5 support
    fixes https://github.com/frontity/wp-plugin/issues/12

---
+++ libs/htmlpurifier-html5/autoload.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once(plugin_dir_path(__FILE__) . '/library/HTMLPurifier/HTML5Config.php');
+require_once(plugin_dir_path(__FILE__) . '/library/HTMLPurifier/HTML5Definition.php');
+require_once(plugin_dir_path(__FILE__) . '/library/HTMLPurifier/AttrDef/Float.php');
+require_once(plugin_dir_path(__FILE__) . '/library/HTMLPurifier/AttrDef/Regexp.php');
+require_once(plugin_dir_path(__FILE__) . '/library/HTMLPurifier/AttrDef/HTML/Bool2.php');
+require_once(plugin_dir_path(__FILE__) . '/library/HTMLPurifier/AttrTransform/Progress.php');
+require_once(plugin_dir_path(__FILE__) . '/library/HTMLPurifier/ChildDef/Details.php');
+require_once(plugin_dir_path(__FILE__) . '/library/HTMLPurifier/ChildDef/Figure.php');
+require_once(plugin_dir_path(__FILE__) . '/library/HTMLPurifier/ChildDef/Media.php');
+require_once(plugin_dir_path(__FILE__) . '/library/HTMLPurifier/ChildDef/Picture.php');
+require_once(plugin_dir_path(__FILE__) . '/library/HTMLPurifier/ChildDef/Progress.php');
+
+?>
