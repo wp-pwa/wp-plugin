31f921814884cb2eef9c660800c3f8eb590a5e57
Luis Herranz <luisherranz@gmail.com>
2018-08-28 14:02:46 +0200

    feat(image-ids): add ids to gallery images using wp_get_attachment_link

--- libs/purifier.php
+++ libs/purifier.php
@@ -5,18 +5,18 @@ function load_purifier() {
   require_once(plugin_dir_path(__FILE__) . '/htmlpurifier-html5/autoload.php');
   
   $config = HTMLPurifier_HTML5Config::createDefault();
-
+  
   $upload = wp_upload_dir();
   $htmlpurifier_dir = $upload['basedir'] . DS . 'frontity' . DS . 'htmlpurifier';
-
+  
   if (is_dir($htmlpurifier_dir)) {
     $config->set('Cache', 'SerializerPath', $htmlpurifier_dir);
-	} else {
+  } else {
     $config->set('Core', 'DefinitionCache', null);
   }
 
   $config->set('HTML.Doctype', 'HTML 4.01 Transitional');
-
+    
   // Rule: All the CSS rules allowed.
   $config->set('CSS.AllowTricky', true);
 
@@ -38,6 +38,10 @@ function load_purifier() {
   $config->set('AutoFormat', 'RemoveEmpty', true);
   $config->set('AutoFormat', 'RemoveEmpty.RemoveNbsp', true);
   $config->set('AutoFormat', 'AutoParagraph', true);
+
+  // IMG Attributes
+  // This is defined in HTML5Definitions.php line 108 (DON'T OVERWRITE IT!!)
+  // $def->addAttribute('img', 'data-attachment-id', 'Text');
 
   return new HTMLPurifier($config);
 }
