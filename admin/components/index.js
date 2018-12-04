cb86ee878cae458c770c66932d874c77300647ac
Eduardo Campaña <asrielo@gmail.com>
2018-12-04 12:16:54 +0100

    chore(grommet): installs grommet
    fixes https://github.com/frontity/wp-plugin/issues/53

--- admin/components/index.js
+++ admin/components/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { string } from "prop-types";
 import { inject } from "mobx-react";
+import { Grommet } from "grommet";
 
-const App = ({ title }) => <div>{title}</div>;
+const App = ({ title }) => <Grommet plain>{title}</Grommet>;
 
 App.propTypes = {
   title: string.isRequired
