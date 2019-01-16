99d4109c54a8f7fe28c2c6a0a9231c2585732bb7
Eduardo Campaña <asrielo@gmail.com>
2019-01-16 16:41:06 +0100

    refactor(wp-pwa): removes dead code

--- wp-pwa.php
+++ wp-pwa.php
@@ -24,9 +24,6 @@ if (!class_exists('frontity')) :
 
 class Frontity {
 	public $plugin_version = '1.10.7';
-	public $rest_api_installed = false;
-	public $rest_api_active = false;
-	public $rest_api_working = false;
 
 	function __construct() {
 		// Migrates settings when the plugin updates.
@@ -43,8 +40,6 @@ class Frontity {
 		// Purges HTMLPurifier cache.
 		add_action('wp_ajax_frontity_purge_htmlpurifier_cache', array($this,'purge_htmlpurifier_cache'));
 
-		add_action('plugins_loaded', array($this, 'wp_rest_api_plugin_is_installed'));
-		add_action('plugins_loaded', array($this, 'wp_rest_api_plugin_is_active'));
 		add_action('plugins_loaded', array($this, 'update_settings'));
 
 		add_action('init', array($this, 'allow_origin'));
@@ -746,60 +741,9 @@ class Frontity {
 		return array('Error' => $last_folder . ' not found');
 	}
 
-	// Checks if the rest-api plugin is installed. I don't think it's used anymore.
-	public function wp_rest_api_plugin_is_installed() {
-		if (!function_exists('get_plugins')) {
-			require_once ABSPATH . 'wp-admin/includes/plugin.php';
-		}
-		$plugins = get_plugins();
-
-		$this->rest_api_installed = isset($plugins['rest-api/plugin.php']);
-	}
-
-	// Checks if the rest-api plugin is active. I don't think it's used anymore.
-	public function wp_rest_api_plugin_is_active() {
-		$this->rest_api_active = class_exists('WP_REST_Controller');
-	}
-
-	// Generates the url to 'auto-activate' the rest-api plugin. I don't think it's used anymore.
-	public function get_activate_wp_rest_api_plugin_url() {
-		$plugin = 'rest-api/plugin.php';
-		$plugin_escaped = str_replace('/', '%2F', $plugin);
-
-		$activateUrl = sprintf(admin_url('plugins.php?action=activate&plugin=%s&plugin_status=all&paged=1&s'), $plugin_escaped);
-
-				// change the plugin request to the plugin to pass the nonce check
-		$_REQUEST['plugin'] = $plugin;
-		$activateUrl = wp_nonce_url($activateUrl, 'activate-plugin_' . $plugin);
-
-		return $activateUrl;
-	}
-
 	// Adds Cross origin * to the header
 	function allow_origin() {
 		if (!headers_sent()) header("Access-Control-Allow-Origin: *");
-	}
-
-	// Checks if the json posts endpoint is responding correctly.  I don't think it's used anymore.
-	function wp_rest_api_endpoint_works() {
-		$rest_api_url = get_site_url() . '/wp-json/wp/v2/posts';
-		$args = array('timeout' => 10, 'httpversion' => '1.1');
-
-		$response = wp_remote_get($rest_api_url, $args);
-
-		if (is_array($response)) {
-			$body = $response['body'];
-			$code = $reponse['reponse']['code'];
-			$message = $reponse['reponse']['message'];
-			$json_reponse = json_decode($body);
-
-			//CHECKS
-			// $code != 200
-			// json valid
-			// json without error message { code: "rest_no_route" }
-		} else {
-			return false;
-		}
 	}
 
 	// Injects the AMP URL to the header.
