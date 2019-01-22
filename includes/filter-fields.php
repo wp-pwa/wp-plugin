208f4b78e87cbb3716cabe11e525abbf52494840
Luis Herranz <luisherranz@gmail.com>
2019-01-22 14:50:26 +0100

    chore(fields): fix a couple of typos in comments

--- includes/filter-fields.php
+++ includes/filter-fields.php
@@ -22,7 +22,7 @@ class Frontity_Filter_Fields
         // We need the postname to enable the filter.
         $post_type_name = $post_type->name;
 
-        // Add de filter. The api uses eg. 'rest_prepare_post' with 3 parameters.
+        // Add the filter. The api uses eg. 'rest_prepare_post' with 3 parameters.
         add_filter('rest_prepare_' . $post_type_name, array($this, 'filter'), 20, 3);
       }
     }
@@ -30,14 +30,14 @@ class Frontity_Filter_Fields
     $tax_types = get_taxonomies(array('public' => true), 'objects');
 
     foreach ($tax_types as $tax_type) {
-      //Test if this posttype should be shown in the rest api.
+      //Test if this taxonomy should be shown in the rest api.
       $show_in_rest = (isset($tax_type->show_in_rest) && $tax_type->show_in_rest) ? true : false;
 
       if ($show_in_rest) {
         // We need the postname to enable the filter.
         $tax_type_name = $tax_type->name;
 
-        // Add de filter. The api uses eg. 'rest_prepare_post' with 3 parameters.
+        // Add the filter. The api uses eg. 'rest_prepare_category' with 3 parameters.
         add_filter('rest_prepare_' . $tax_type_name, array($this, 'filter'), 20, 3);
       }
     }
