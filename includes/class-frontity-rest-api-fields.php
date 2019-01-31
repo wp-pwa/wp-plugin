9bf7890a302ee14baf19e8b15064202560d35acc
Eduardo Campaña <asrielo@gmail.com>
2019-01-31 17:21:00 +0100

    refactor(rest-api-fields): fixes method get_latest

--- includes/class-frontity-rest-api-fields.php
+++ includes/class-frontity-rest-api-fields.php
@@ -23,7 +23,7 @@ class Frontity_Rest_Api_Fields
   
   // Returns the value for the `latest` field in post_type,
   // and sets a hook to filter it.
-  function wp_api_get_latest($post_type)
+  function get_latest($post_type)
   {
     return apply_filters('add_custom_post_types_to_latest', array($post_type['type']));
   }
